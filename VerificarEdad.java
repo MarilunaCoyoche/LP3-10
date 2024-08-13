e8f9cdf910b83716495a39d43f2628c808fc829c
MarilunaCoyoche <101837277+MarilunaCoyoche@users.noreply.github.com>
2024-08-13 10:08:35 -0500

    Create VerificarEdad.java

---
+++ VerificarEdad.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VerificarEdad {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+        
+        if (edad >= 18) {
+            System.out.println("Mayor de edad");
+        } else {
+            System.out.println("Menor de edad");
+        }
+        
+        scanner.close();
+    }
+}
